43460706b0f6900e6bd6211e0de1227ada4de267
sceiiya <124661065+sceiiya@users.noreply.github.com>
2023-05-23 16:20:46 +0800

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Testing CJS Laravel + TailwindCSS</title>
+        @vite('resources/css/app.css')
+    </head>
+    <body class="">
+        <h1 class='pt-8 bg-red-800 text-center w-full text-white text-lg font-extrabold'>
+            Test TailwindCSS!
+        </h1>
+        <p>If the changes in classes is not changing the design, go to your terminal na nagrrun ng npm run dev.. press q then enter command npm run dev lang ulit</p>
+    </body>
+</html>
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './index.html', './src/**/*.{js,ts,jsx,tsx}', '*.html', '*.php', '*.jsx',
+    "./resources/**/*.blade.php",
+    "./resources/**/*.js",
+    "./resources/**/*.vue",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
