e54e9a66b613dfd2d1d12e703ea5ccfa172811b6
sceiiya <124661065+sceiiya@users.noreply.github.com>
2023-05-28 07:38:02 +0800

    add migration table employee

---
+++ database/migrations/2023_05_27_225819_create_employees_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('employees', function (Blueprint $table) {
+            $table->id();
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('middle_name')->nullable();
+            $table->string('email')->unique();
+            $table->string('password');
+            $table->timestamp('applied_at')->nullable();
+            $table->timestamp('joined_at')->nullable();
+            $table->timestamp('archived_at')->nullable();
+            $table->string('status')->nullable();
+            $table->string('otp')->nullable();
+            $table->string('job_position')->nullable();
+            $table->string('job_type')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('employees');
+    }
+};
