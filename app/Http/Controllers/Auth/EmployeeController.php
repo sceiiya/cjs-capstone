f983142ae20dc9269a91a7684b16e361aaf866dc
sceiiya <124661065+sceiiya@users.noreply.github.com>
2023-05-28 07:42:45 +0800

    add controller enemy

---
+++ app/Http/Controllers/Auth/EmployeeController.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Models\Employee;
+use Illuminate\Http\Request;
+
+class EmployeeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $employees = Employee::all();
+        return view('employees.index', compact('employees'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('employees.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        // Validate the request data
+        $request->validate([
+            'first_name' => 'required',
+            'last_name' => 'required',
+            // Add validation rules for other fields
+        ]);
+
+        // Create a new employee record
+        Employee::create($request->all());
+
+        // Redirect to the index page with success message
+        return redirect()->route('employees.index')->with('success', 'Employee created successfully');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Employee $employee)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Employee $employee)
+    {
+        return view('employees.edit', compact('employee'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Employee $employee)
+    {
+        // Validate the request data
+        $request->validate([
+            'first_name' => 'required',
+            'last_name' => 'required',
+            // Add validation rules for other fields
+        ]);
+
+        // Update the employee record
+        $employee->update($request->all());
+
+        // Redirect to the index page with success message
+        return redirect()->route('employees.index')->with('success', 'Employee updated successfully');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Employee $employee)
+    {
+        // Delete the employee record
+        $employee->delete();
+
+        // Redirect to the index page with success message
+        return redirect()->route('employees.index')->with('success', 'Employee deleted successfully');
+    }
+}
