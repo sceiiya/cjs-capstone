54b05e8b5f3b59006cdf4d040fbb82a29405b6c3
sceiiya <124661065+sceiiya@users.noreply.github.com>
2023-05-28 15:34:54 +0800

    try swipe library

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -12,5 +12,12 @@
             Test TailwindCSS!
         </h1>
         <p class="p-8 text-white bg-slate-700 ">If the changes in classes is no t changing the design, go to your terminal na nagrrun ng npm run dev.. press q then enter command npm run dev lang ulit</p>
+        <script src="https://cdn.jsdelivr.net/npm/swiper@9/swiper-element-bundle.min.js"></script>
+<swiper-container class="bg-green-200 p-2 m-4">
+  <swiper-slide class="bg-violet-900">Slide 1</swiper-slide>
+  <swiper-slide class="bg-red-600">Slide 2</swiper-slide>
+  <swiper-slide class="bg-green-500">Slide 3</swiper-slide>
+  <swiper-slide class="bg-orange-600">Slide ...</swiper-slide>
+</swiper-container>
     </body>
 </html>
