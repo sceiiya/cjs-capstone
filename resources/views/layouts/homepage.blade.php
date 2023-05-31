0fbcd8f16748ed4ad7f1f3815f4d8ecf2cf0ddd3
sceiiya <124661065+sceiiya@users.noreply.github.com>
2023-05-31 21:00:53 +0800

    save safe first

--- resources/views/layouts/homepage.blade.php
+++ resources/views/layouts/homepage.blade.php
@@ -5,13 +5,12 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="_csrf-token" content="{{csrf_token()}}">
 
-
         <title>{{$title}}</title>
         @vite('resources/css/app.css')
     </head>
     <body class="">
         <h1 class='pt-8 bg-red-800 text-center w-full text-white text-lg font-extrabold'>
-            Test Employee Model CRUD BOO!
+            Test Homepage Layout BOO!
         </h1>
         <!-- ---------------============== playground ==============--------------- -->
         <section class="flex flex-col justify-center p-3 ml-5">
@@ -22,66 +21,6 @@
         </div>
         <!-- Employee Table -->
         <div id="employeeTable">
-
-            <table class="table-auto table">
-                <tr>
-                    <th>first_name</th>
-                    <th>last_name</th>
-                    <th>middle_name</th>
-                    <th>username</th>
-                    <th>email</th>
-                    <th>applied_at</th>
-                    <th>joined_at</th>
-                    <th>archived_at</th>
-                    <th>status</th>
-                    <th>otp</th>
-                    <th>profile_pic</th>
-                    <th>job_position</th>
-                    <th>job_type</th>
-                    <th>country</th>
-                    <th>city</th>
-                    <th>province_state</th>
-                    <th>street</th>
-                    <th>postal_id</th>
-                    <th>google_id</th>
-                    <th>Edit<th>
-                    <th>Delete</th>
-                </tr>
-                    @if(!empty($employees))
-                    @foreach($employees as $employee)
-                        <tr>   
-                            <td> {{$employee->first_name}} </td>
-                            <td> {{$employee->last_name}} </td>
-                            <td> {{$employee->middle_name}} </td>
-                            <td> {{$employee->username}} </td>
-                            <td> {{$employee->email}} </td>
-                            <td> {{$employee->applied_at}} </td>
-                            <td> {{$employee->joined_at}} </td>
-                            <td> {{$employee->archived_at}} </td>
-                            <td> {{$employee->status}} </td>
-                            <td> {{$employee->otp}} </td>
-                            <td> {{$employee->profile_pic}} </td>
-                            <td> {{$employee->job_position}} </td>
-                            <td> {{$employee->job_type}} </td>
-                            <td> {{$employee->country}} </td>
-                            <td> {{$employee->city}} </td>
-                            <td> {{$employee->province_state}} </td>
-                            <td> {{$employee->street}} </td>
-                            <td> {{$employee->postal_id}} </td>
-                            <td> {{$employee->google_id}} </td>
-                            <td><a href="{{route('employee.edit', [ 'employee' => $employee ])}}">Edit</a></td>
-                            <td>
-    <form action="{{ route('employee.delete', ['employee' => $employee]) }}" method="POST">
-        @csrf
-        @method('PUT')
-        <button type="submit">Delete</button>
-    </form>
-</td>
-
-                        </tr>
-                        @endforeach
-                        @endif
-            </table>
 
         </div>
 
@@ -189,36 +128,6 @@
             <button type="submit">Create Employee</button>
         </form>
         @endif
-
-        <!-- Edit Employee Modal -->
-        <div class="modal fade" id="editModal" tabindex="-1" role="dialog" aria-labelledby="editModalLabel" aria-hidden="true">
-            <div class="modal-dialog" role="document">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <h5 class="modal-title" id="editModalLabel">Edit Employee</h5>
-                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                            <span aria-hidden="true">&times;</span>
-                        </button>
-                    </div>
-                    <form id="editEmployeeForm" method="POST" action="{{ route('employees.update', ':id') }}">
-                        @csrf
-                        @method('PUT')
-                        <div class="modal-body">
-                            <!-- Hidden field for employee ID -->
-                            <input type="hidden" name="employee_id" id="editEmployeeId">
-                            <!-- Edit form fields -->
-                            <input type="text" name="first_name" id="editFirstName" placeholder="First Name" required>
-                            <input type="text" name="last_name" id="editLastName" placeholder="Last Name" required>
-                            <!-- Add other fields as needed -->
-                        </div>
-                        <div class="modal-footer">
-                            <button type="submit" class="btn btn-primary">Save Changes</button>
-                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                        </div>
-                    </form>
-                </div>
-            </div>
-        </div>
 
         <!-- Success message -->
         <div id="successMessage" style="display: none;"></div>
