0bd01cc15734fdd30e72ab04375c356a5789729b
sceiiya <124661065+sceiiya@users.noreply.github.com>
2023-06-08 16:05:30 +0800

    delete unnecessary middlewares

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -54,10 +54,7 @@ class Kernel extends HttpKernel
      */
     protected $middlewareAliases = [
         'auth' => \App\Http\Middleware\Authenticate::class,
-        'authApplicant' => \App\Http\Middleware\AuthenticateApplicant::class,
-        'authEmployee' => \App\Http\Middleware\AuthenticateEmployee::class,
-        'authAdmin' => \App\Http\Middleware\AuthenticateAdmin::class,
-        'authRole' => \App\Http\Middleware\AuthenticateAdmin::class,
+        'authRole' => \App\Http\Middleware\AuthenticateRole::class,
         'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
         'auth.session' => \Illuminate\Session\Middleware\AuthenticateSession::class,
         'cache.headers' => \Illuminate\Http\Middleware\SetCacheHeaders::class,
