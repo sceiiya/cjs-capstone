2ce6b077aef03bb53f9d8ceb7edb40b73420c150
sceiiya <124661065+sceiiya@users.noreply.github.com>
2023-06-09 11:15:45 +0800

    add blade conditional ref role

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +20,16 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Blade::if('admin', function () {
+            return auth()->check() && auth()->user()->role == 'admin';
+        });
+
+        Blade::if('employee', function () {
+            return auth()->check() && auth()->user()->role == 'employee';
+        });
+
+        Blade::if('applicant', function () {
+            return auth()->check() && auth()->user()->role == 'applicant';
+        });
     }
 }
