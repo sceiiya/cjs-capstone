72a63c6d1586d9da1606f63b7e08a2246ef64e4e
sceiiya <124661065+sceiiya@users.noreply.github.com>
2023-06-10 11:57:57 +0800

    fix links

--- resources/views/layouts/page.blade.php
+++ resources/views/layouts/page.blade.php
@@ -8,7 +8,8 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;0,1000;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900;1,1000&display=swap" rel="stylesheet">
     <title>{{config('app.name', 'Laravel').' | '.$title}}</title>
-    @vite(['resources/js/jquery-3.7.0.min.js', 'resources/css/app.css'])
+    <script src="{{asset('src/js/jquery-3.7.0.min.js')}}"></script>
+    @vite(['resources/css/app.css'])
 </head>
 <body class="bg-emerald-100 hr-solutions-bg nunito-font">
     <div id="app" class="flex flex-col h-screen">
